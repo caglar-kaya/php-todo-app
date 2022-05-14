0679bb004a2b07be87b6fc854d5943f1d5d11d8f
Caglar Kaya <C.kaya@Intermax.nl>
2022-05-14 12:30:55 +0200

    separate php and html for create file

---
+++ public/create.view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Todo App</title>
+    </head>
+    <body>
+    <a href="index.php">
+        <button type="button">Go to Todos!</button>
+    </a>
+    </body>
+</html>
