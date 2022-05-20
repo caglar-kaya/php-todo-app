d4aca2b146654c4c78cd4fb68d49be4ce6c5cf2f
Caglar Kaya <C.kaya@Intermax.nl>
2022-05-20 11:25:51 +0200

    use composer autoloading

--- public/index.php
+++ public/index.php
@@ -4,7 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include '../router/Router.php';
+include '../vendor/autoload.php';
+
 include '../router/routes.php';
 
 $router = new Router();
